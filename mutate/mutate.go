221f7eb4f37a145dd3309fc7ca2067e43ef2874b
James Landrein <james.landrein@polyconseil.fr>
2021-07-28 11:35:47 +0200

    mutate: refacto and fix tests accordingly

--- mutate/mutate.go
+++ mutate/mutate.go
@@ -12,46 +12,106 @@ import (
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
-func GetPatchedImageUrl(img, registry string) string {
-	if img == "registry" ||
-	   strings.HasPrefix(img, "registry:") ||
-	   strings.Contains(img, "/registry:") {
-		return img
+// registry/namespace/image:tag
+type DockerImageUrl struct {
+	registry string
+	namespace string
+	image string
+	tag string
+}
+
+// takes toto:tata or toto, gives tata or latest
+func getImgTag(img string) string {
+	imgTagArr := strings.Split(img, ":")
+	tag := "latest"
+	if len(imgTagArr) == 2 {
+		tag = imgTagArr[1]
 	}
 
+		return tag
+}
+
+// takes toto:tata or toto, gives toto
+func getImgName(img string) string {
+		imgTagArr := strings.Split(img, ":")
+		return imgTagArr[0]
+}
+
+func getDockerImageUrl(img string) DockerImageUrl {
 	imgArr := strings.Split(img, "/")
 	// Not prefixed with a site
 	if len(imgArr) == 1 {
 		// Case busybox or busybox:tag
-		return fmt.Sprintf("%s/library/%s", registry, img)
+		return DockerImageUrl{
+			registry: "docker.io", // default
+			namespace: "library", // default
+			image: getImgName(img),
+			tag: getImgTag(img),
+		}
 	}
 
 	imgUrl := imgArr[0]
-	imgName := strings.Join(imgArr[1:], "/")
 	// Case docker.io/busybox
 	if len(imgArr) == 2 && imgUrl == "docker.io" {
-		return fmt.Sprintf("%s/library/%s", registry, imgName)
+		return DockerImageUrl {
+			registry: imgUrl,
+			namespace: "library",
+			image: getImgName(imgArr[1]),
+			tag: getImgTag(imgArr[1]),
+		}
 	}
 
 	// Case toto/tata (and ! gcr.io/toto)
 	if len(imgArr) == 2 && !strings.Contains(imgUrl, ".") {
-		if img != "library/registry" &&
-		   !strings.HasPrefix(img, "library/registry:") &&
-		   !strings.Contains(img, "/library/registry:") {
-			return fmt.Sprintf("%s/%s", registry, strings.Join(imgArr, "/"))
+		return DockerImageUrl {
+			registry: "docker.io",
+			namespace: imgArr[0],
+			image: getImgName(imgArr[1]),
+			tag: getImgTag(imgArr[1]),
 		}
-		return img
 	}
 
-	// Case docker.io/toto/tata
-	if imgUrl == "docker.io" {
-		if img != "docker.io/library/registry" &&
-		!strings.HasPrefix(img, "docker.io/library/registry:") {
-			return fmt.Sprintf("%s/%s", registry, imgName)
+	if len(imgArr) == 2 && strings.Contains(imgUrl, ".") {
+	return DockerImageUrl {
+			registry: imgUrl,
+			namespace: "", // ??? TODO does it exist?
+			image: getImgName(imgArr[1]),
+			tag: getImgTag(imgArr[1]),
 		}
-		return img
 	}
-	return img
+
+	// case toto.io/tata/titi[:tag]
+	return DockerImageUrl {
+		registry: imgArr[0],
+		namespace: imgArr[1],
+		image: getImgName(imgArr[2]),
+		tag: getImgTag(imgArr[2]),
+	}
+}
+
+func (i DockerImageUrl) String() string {
+	if i.namespace == "" {
+		return fmt.Sprintf("%s/%s:%s",
+			i.registry,
+			i.image,
+			i.tag)
+	}
+	return fmt.Sprintf("%s/%s/%s:%s",
+		i.registry,
+		i.namespace,
+		i.image,
+		i.tag)
+}
+
+func GetPatchedImageUrl(img, registry string) string {
+	patchimg := getDockerImageUrl(img)
+
+	if patchimg.registry == "docker.io" &&
+		patchimg.image != "registry" {
+		patchimg.registry = registry
+	}
+
+	return patchimg.String()
 }
 
 func getPatchFromContainerList(ctn []corev1.Container, registry, containerType string) []map[string]string {
@@ -108,7 +168,6 @@ func Mutate(body []byte, verbose bool, registry string) ([]byte, error) {
 
 		return responseBody, nil
 	}
-
 
 	if err := json.Unmarshal(ar.Object.Raw, &pod); err != nil {
 		log.Println("FATAL Error ", err)
